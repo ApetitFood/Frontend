0eaaeb0b6ab1b2b6a59bfb342fe28506cc7319b3
Kimblis <E.Masilionis122@gmail.com>
2022-03-17 17:00:53 +0200

    complete registration logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import type { NextPage } from 'next'
-import { Container } from '@chakra-ui/react'
+import { Container, Heading } from '@chakra-ui/react'
 
-const Home: NextPage = () => <Container maxW='container.l' p={0}></Container>
+const Home: NextPage = () => (
+  <Container maxW='container.l' p={0}>
+    <Heading size='4xl' color='teal'>
+      This is very beutiful main page
+    </Heading>
+  </Container>
+)
 
 export default Home

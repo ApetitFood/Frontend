7626821144ed5e92221fc2cb065724ea0a85dc1c
TizQ <linas.janusauskas@ktu.edu>
2022-03-22 21:33:35 +0200

    Menu added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next'
-import { Container, Heading } from '@chakra-ui/react'
+import { Container, Heading } from "@chakra-ui/react"
+import Header from '../src/components/Header'
+import Footer from '../src/components/Footer'
 
 const Home: NextPage = () => (
   <Container maxW='container.l' p={0}>
+    <Header/>
     <Heading size='4xl' color='teal'>
       This is very beutiful main page
     </Heading>
+    <Footer/>
   </Container>
 )
 
